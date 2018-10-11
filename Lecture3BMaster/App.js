da6676737abf31e5adae5fc6e3a050c131ad51d8
joshuaojo03 <35880780+joshuaojo03@users.noreply.github.com>
2018-10-11 11:07:23 -0700

    Add files via upload

--- Lecture3BMaster/App.js
+++ Lecture3BMaster/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, FlatList, TextInput, Button, SafeAreaView } from 'react-native';
-import { Card, CheckBox } from 'react-native-elements';
-import  Modal from 'react-native-modal';
-import Metrics from './Themes/Metrics';
-import { WebBrowser, MapView, Location, Permissions } from 'expo';
 import ModalView from './App/Components/ModalView';
 import MapComponent from './App/Components/MapComponent';
-import { FontAwesome, Feather, MaterialCommunityIcons } from '@expo/vector-icons';
 import VectorIcons from './App/Components/VectorIcons';
 import WebComponent from './App/Components/WebComponent';
 
@@ -15,14 +10,6 @@ export default class App extends React.Component {
 constructor(props) {
   super(props);
   this.state ={
-    checked: false,
-    isModalVisible: false,
-    isWebModalVisible: false,
-    googleSearch: '',
-    randomThings: [],
-    mapRegion: null,
-    text: '',
-    showMap: false,
   }
 }
 
